78d259dd308d98219928d5cbe0a6340b9bb25c03
Jeevan-Josh <merakanapallijeevanjosh@gmail.com>
2026-08-10 16:44:25 +0530

    Create jobCard.js

---
+++ Day10/Sprint-10-LWC-Component-Communication/Job-Card/jobCard.js
@@ -0,0 +1,29 @@
+import { LightningElement, api } from 'lwc';
+
+export default class JobCard extends LightningElement {
+
+    @api job;
+    @api isApplying;
+
+    handleViewDetails() {
+
+        this.dispatchEvent(
+            new CustomEvent('viewdetails', {
+                detail: {
+                    jobId: this.job.id
+                }
+            })
+        );
+    }
+
+    handleApply() {
+
+        this.dispatchEvent(
+            new CustomEvent('apply', {
+                detail: {
+                    jobId: this.job.id
+                }
+            })
+        );
+    }
+}
